969a71a578dce89ead83a562d0fa9ac83dc2a4f9
Bladeage <fageyer@posteo.de>
2024-07-29 11:05:36 +0200

    Added german language

---
+++ map_german.php
@@ -0,0 +1,41 @@
+<?php
+
+$character_race = array(
+  1 => 'Mensch',
+  2 => 'Orc',
+  3 => 'Zwerg',
+  4 => 'Nachtelf',
+  5 => 'Untoter',
+  6 => 'Tauren',
+  7 => 'Gnom',
+  8 => 'Troll',
+  9 => 'Goblin',
+  10 => 'Blutelf',
+  11 => 'Draenei');
+
+$character_class = array(
+  1 => 'Krieger',
+  2 => 'Paladin',
+  3 => 'Jäger',
+  4 => 'Schurke',
+  5 => 'Priester',
+  6 => 'Todesritter',
+  7 => 'Schamane',
+  8 => 'Magier',
+  9 => 'Hexenmeister',
+  11 => 'Druide');
+
+$lang_defs = array(
+  'maps_names' => array('Azeroth', 'Scherbenwelt', 'Nordend'),
+  'total' => 'Gesamt',
+  'faction' => array('Allianz', 'Horde'),
+  'name' => 'Name',
+  'race' => 'Rasse',
+  'class' => 'Klasse',
+  'level' => 'Lvl',
+  'click_to_next' => 'Klicken: Weiter',
+  'click_to_first' => 'Klicken: Zum Anfang'
+);
+
+include "zone_names_german.php";
+?>
